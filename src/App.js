1f267390fb428846648672f5636242fdfbd92907
raloliver <raloliver.ws@gmail.com>
2023-08-18 13:53:22 +0100

    useState with input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState('');
 
   function increaseCounter() {
     setCounter(counter + 1);
@@ -9,6 +10,11 @@ const App = () => {
 
   return (
     <div>
+      <h3>{input}</h3>
+      <input
+        type="text"
+        onChange={(event) => setInput(event.target.value)}
+      />
       <h1>Counter: {counter}</h1>
       <button onClick={increaseCounter}>Increase</button>
     </div>
