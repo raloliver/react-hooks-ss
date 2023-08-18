edfa80a20b15bf0eeca01193ca355528510a9ca9
israelloliveira <israel.oliveira@doutorfinancas.pt>
2023-08-18 13:43:42 +0100

    useState with button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  const [counter, setCounter] = useState(0);
+
+  function increaseCounter() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increaseCounter}>Increase</button>
+    </div>
+  );
 };
 
 export default App;
