39368bc9bc69de5e8355d9cd5ba3e421f67b2f97
raloliver <raloliver.ws@gmail.com>
2023-09-18 05:54:36 +0100

    add error handle

--- src/HttpClient.js
+++ src/HttpClient.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 
 export const HttpClient = () => {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://dummyjson.com/products')
-      .then(({ data }) => setProducts(data.products));
+      .then(({ data }) => setProducts(data.products))
+      .catch(({ message }) => setError(message));
   });
   return (
     <>
+      {error && error}
       {products.map((product, index) => {
         return (
           <h3 key={index}>
